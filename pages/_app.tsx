541bba3b31cad4253e08b8601506c7fe54b7d440
intersoom <sallybig@naver.com>
2023-07-25 02:26:19 +0900

    Fix: AppProps 주소 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from '@/../../../채연/SSU-IT/Soongcha-ro/node_modules/next/app';
+import { AppProps } from 'next/app';
 import { GlobalStyle } from '../styles/globalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
