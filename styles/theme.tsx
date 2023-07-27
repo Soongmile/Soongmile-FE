b12ceecbd22932af7539809c02886b2b7ec70a8d
seocylucky <chaeyeon2285@naver.com>
2023-07-27 16:39:27 +0900

    [#7] Feat: 디자인 시스템 theme 작업

---
+++ styles/theme.tsx
@@ -0,0 +1,53 @@
+const theme = {
+    colors: {
+        white: '#FFFFFF',
+        black: '#1D1D1D',
+        primary: '#006794',
+        secondary: '#E0EEFF',
+        tertiary: '#D3E7FE',
+        footer: '#0C4964',
+        hovered: '#005174',
+        primary_disabled: 'rgba(0, 103, 148, 0.50)',
+        primary_tag: 'rgba(0, 103, 148, 0.08)',
+        black_tag: 'rgba(29, 29, 29, 0.08)',
+        gray0: '#000000',
+        gray1: '#F2F4F6',
+        gray2: '#B0B0B0',
+        gray3: '#707070',
+        error: '#B3261E',
+        alert: '#F66570',
+        
+    },
+    fontStyles: {
+        Title_L: {
+            fontSize: 48,
+            fontWeight: 600,
+          },
+          Title_M: {
+            fontSize: 36,
+            fontWeight: 600,
+          },
+          Title_S: {
+            fontSize: 24,
+            fontWeight: 600,
+          },
+          Text_L: {
+            fontSize: 18,
+            fontWeight: 600,
+          },
+          Text_M: {
+            fontSize: 18,
+            fontWeight: 400,
+          },
+          Text_S: {
+            fontSize: 14,
+            fontWeight: 400,
+          },
+          Caption: {
+            fontSize: 12,
+            fontWeight: 400,
+          },
+    }
+}
+
+export default theme
