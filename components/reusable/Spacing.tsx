8a0a07dbcb57a6f4159c3d72ca9d1f5dbfef9989
intersoom <sallybig@naver.com>
2023-07-27 16:39:17 +0900

    [#2] feat: 카드형 컴포넌트 제작

--- components/reusable/Spacing.tsx
+++ components/reusable/Spacing.tsx
@@ -1,5 +1,5 @@
-import StyledSpacing from '@/styles/StyledSpacing';
 import { HTMLAttributes, memo } from 'react';
+import styled from 'styled-components';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   children?: never;
@@ -10,3 +10,14 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 export const Spacing = memo(function Spacing({ direction = 'vertical', size, ...props }: Props) {
   return <StyledSpacing direction={direction} size={size} />;
 });
+
+interface StyledSpacingProps {
+  readonly direction: 'horizontal' | 'vertical';
+  readonly size: number;
+}
+
+const StyledSpacing = styled.div<StyledSpacingProps>`
+  flex: none;
+  width: ${(props) => (props.direction === 'horizontal' ? `${props.size}px` : undefined)};
+  height: ${(props) => (props.direction === 'vertical' ? `${props.size}px` : undefined)};
+`;
--- components/reusable/Tag.tsx
+++ components/reusable/Tag.tsx
@@ -1,13 +1,46 @@
-import StyledTag from '@/styles/StyledTag';
+import colors from '@/styles/colors';
+import styled from 'styled-components';
 
 interface Props extends React.HTMLAttributes<HTMLSpanElement> {
   readonly variant?: 'color' | 'gray';
+  readonly content: string;
 }
 
-export function Tag({ variant = 'color', ...props }: Props) {
+export function Tag({ variant = 'color', content, ...props }: Props) {
   return (
     <StyledTag variant={variant} {...props}>
-      내용
+      {content}
     </StyledTag>
   );
 }
+
+interface StyledTagProps {
+  readonly variant: 'color' | 'gray';
+}
+
+const StyledTag = styled.span<StyledTagProps>`
+  background-color: ${(props) => backgroundColor[props.variant]};
+  color: ${(props) => textColor[props.variant]};
+  border-radius: ${(props) => radiusSize[props.variant]};
+  height: 24px;
+  min-width: fit-content;
+  font-size: 12px;
+  border: none;
+  padding: 5px 12px;
+  margin: 0;
+`;
+
+const backgroundColor = {
+  color: colors.primarybg,
+  gray: colors.graybg,
+};
+
+const textColor = {
+  color: colors.primary,
+  gray: colors.gray3,
+};
+
+const radiusSize = {
+  color: '8px',
+  gray: '100px',
+};
