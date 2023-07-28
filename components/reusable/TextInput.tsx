62129f842f3714d16a22f81d805cd825f29add1e
seocylucky <seocylucky@github.com>
2023-07-29 04:11:06 +0900

    Fix: colors theme으로 부르도록 수정

--- components/reusable/TextInput.tsx
+++ components/reusable/TextInput.tsx
@@ -37,33 +37,33 @@ const InputBox = styled.input<{ width: string; height: string; }>`
   height: ${({ height }) => height};
   padding-left: 24px;
   border-radius: 16px;
-  border: 1px solid ${colors.gray2};
+  border: 1px solid ${theme.colors.gray2};
   font-weight: ${theme.fontStyles.Text_M.fontWeight};
   font-size: ${theme.fontStyles.Text_M.fontSize}px;
   
   &::placeholder {
-    color: ${colors.gray3};
+    color: ${theme.colors.gray3};
     font-weight: ${theme.fontStyles.Text_M.fontWeight};
     font-size: ${theme.fontStyles.Text_M.fontSize}px;
   }
 
   &:hover {
-    border-color: ${colors.gray3};
+    border-color: ${theme.colors.gray3};
   }
 
   &:focus {
-    outline: 1px solid ${colors.primary};
+    outline: 1px solid ${theme.colors.primary};
     box-shadow: 0px 1px 3px 1px rgba(176, 176, 176, 0.50);
   }
 
   &.has-error {
-    outline: 1px solid ${colors.error};
+    outline: 1px solid ${theme.colors.error};
   }
 `;
 
 const ErrorMessage = styled.div`
   margin-top: 16px;
-  color: ${colors.error};
+  color: ${theme.colors.error};
   font-weight: ${theme.fontStyles.Text_S.fontWeight};
   font-size: ${theme.fontStyles.Text_S.fontSize}px;
 `;
