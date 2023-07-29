265ac060b986dd8f983c12bdb6f538f8ef910eed
intersoom <sallybig@naver.com>
2023-07-29 18:29:16 +0900

    Etc: 충돌 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
-<<<<<<< Updated upstream
-import type { NextPage } from 'next'
-=======
 import type { NextPage } from 'next';
-import Card from '@/components/reusable/Card';
->>>>>>> Stashed changes
 
 const Home: NextPage = () => {
-  return (
-    <h1>Home Page</h1>
-  )
-}
+  return <h1>Home Page</h1>;
+};
 
-export default Home
+export default Home;
