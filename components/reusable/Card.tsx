191952d29b1186fb35ca1e23a0b95f1feb685046
intersoom <sallybig@naver.com>
2023-08-01 17:20:19 +0900

    [#2] Refactor: theme import 추가

--- components/reusable/Card.tsx
+++ components/reusable/Card.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import colors from '@/styles/colors';
+import theme from '@/styles/theme';
 import Spacing from '@/components/reusable/Spacing';
 import Tag from '@/components/reusable/Tag';
 
