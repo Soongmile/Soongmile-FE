abfc01bc5ef7d6fdcea8a112cac62ac82da9113a
seocylucky <chaeyeon2285@naver.com>
2023-08-23 22:51:47 +0900

    [#33] Fix: Header width 수정 및 svg 버그 개선

--- components/reusable/Header.tsx
+++ components/reusable/Header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { styled } from 'styled-components';
 import theme from '../../styles/theme';
 import { usePathname } from 'next/navigation';
+import Logo from '../../assets/Logo.svg';
 
 interface LinkProps {
   active: boolean;
@@ -11,31 +12,33 @@ const Header = () => {
   const pathname = usePathname();
   return (
     <Container>
-      <Link href="/">
-        <LogoImg />
-      </Link>
-      <Nav>
-        <NavLink active={pathname === '/'} href="/">
-          홈
-        </NavLink>
-        <NavLink active={pathname === '/questionWrite'} href="/questionWrite">
-          질문
-        </NavLink>
-        <NavLink active={pathname === '/'} href="/">
-          포인트
-        </NavLink>
-        <NavLink active={pathname === '/'} href="/">
-          커피챗
-        </NavLink>
-      </Nav>
-      <Nav>
-        <NavLink active={pathname === '/signin'} href="/signin">
-          로그인
-        </NavLink>
-        <NavLink active={pathname === '/signup'} href="/signup">
-          회원가입
-        </NavLink>
-      </Nav>
+      <Content>
+        <Link href="/">
+          <Logo />
+        </Link>
+        <Nav>
+          <NavLink active={pathname === '/'} href="/">
+            홈
+          </NavLink>
+          <NavLink active={pathname === '/questionWrite'} href="/questionWrite">
+            질문
+          </NavLink>
+          <NavLink active={pathname === '/'} href="/">
+            포인트
+          </NavLink>
+          <NavLink active={pathname === '/'} href="/">
+            커피챗
+          </NavLink>
+        </Nav>
+        <Nav>
+          <NavLink active={pathname === '/signin'} href="/signin">
+            로그인
+          </NavLink>
+          <NavLink active={pathname === '/signup'} href="/signup">
+            회원가입
+          </NavLink>
+        </Nav>
+      </Content>
     </Container>
   );
 };
@@ -43,22 +46,20 @@ const Header = () => {
 export default Header;
 
 const Container = styled.div`
-  padding-left: 155px;
-  padding-right: 155px;
   width: 100%;
   height: 72px;
+  background-color: ${theme.colors.white};
+  border-bottom: 2px solid ${theme.colors.gray1};
+`;
+
+const Content = styled.div`
+  width: 1120px;
+  margin: 0 auto;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  background-color: ${theme.colors.white};
-  border-bottom: 2px solid ${theme.colors.gray1};
   align-items: center;
-`;
-
-const LogoImg = styled.div`
-  width: 72px;
   height: 72px;
-  background-image: url('Logo.svg');
 `;
 
 const Nav = styled.div`
