760a0c2e4a1fdb18ba2305e315ca70968963e678
seocylucky <seocylucky@github.com>
2023-09-18 01:32:17 +0900

    Fix: 빌드 오류 문제 해결

--- components/reusable/Header.tsx
+++ components/reusable/Header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import { styled } from 'styled-components';
 import { usePathname } from 'next/navigation';
+import { styled } from 'styled-components';
 import theme from '../../styles/theme';
 import Logo from '../../assets/Logo.svg';
 
