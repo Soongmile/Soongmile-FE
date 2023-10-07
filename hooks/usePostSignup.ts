cd7b3e264bec0d667cebb1432a0f4b3cd64b2bd6
seocylucky <seocylucky@github.com>
2023-10-07 22:51:25 +0900

    feat: usePostSignup 훅 작업

---
+++ hooks/usePostSignup.ts
@@ -0,0 +1,20 @@
+import postSignUp from '@/apis/postSignup';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/router';
+import { useMutation } from 'react-query';
+
+const usePostSignup = () => {
+  const router = useRouter();
+  return useMutation(postSignUp, {
+    onSuccess: () => {
+      router.push('/');
+    },
+    onError: (error) => {
+      const Error = error as AxiosError;
+
+      console.log(Error);
+    },
+  });
+};
+
+export default usePostSignup;
